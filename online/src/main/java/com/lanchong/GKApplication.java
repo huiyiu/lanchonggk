7011be43939f8b070b6dd35ecd8fe053e7508963
huiyi <huiyisn@163.com>
2019-02-23 21:00:24 +0800

    加密修改

--- online/src/main/java/com/lanchong/GKApplication.java
+++ online/src/main/java/com/lanchong/GKApplication.java
@@ -1,14 +1,10 @@
 package com.lanchong;
 
-import org.jasypt.encryption.StringEncryptor;
-import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
-import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.servlet.MultipartConfigElement;
@@ -45,15 +41,5 @@ public class GKApplication {
         }
         factory.setLocation(realPath);
         return factory.createMultipartConfig();
-    }
-
-    @Bean("stringEncryptor")
-    public StringEncryptor stringEncryptor() {
-        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(jasyptPwd);
-        config.setPoolSize("1");
-        encryptor.setConfig(config);
-        return encryptor;
     }
 }
